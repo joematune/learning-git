ca70fbad1fe92b53a91bc6dbfbf8ff025f30883f
joematune <joematune@gmail.com>
2019-11-16 19:31:08 +0100

    Restrict polyline to card bounding box

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ console.log('happy');
 var vw = window.innerWidth / 100;
 var vh = window.innerHeight / 100;
 
-function makePoints(num) {
+function makePoints(num, minX, width, minY, height) {
     let points = '';
     for (i = 0; i <= num; i++) {
-        let y = Math.floor(Math.random() * 100*vh);
-        let x = Math.floor(Math.random() * 100*vw);
+        let y = minY + Math.floor(Math.random() * height -5);
+        let x = minX + Math.floor(Math.random() * width -5);
         points += `${x},${y} `
     }
     return points;
 }
 
-let points = makePoints(45);
+// get card bounding box
+
+let box = document.querySelector('main').getBoundingClientRect();
+
+// let points = makePoints(45, 0, 100*vw, 0, 100*vh); fill whole screen
+let points = makePoints(45, box.x, box.width, box.y, box.height);
 let polyline = document.querySelector('polyline');
 polyline.setAttribute('points', points);
 
@@ -32,10 +37,10 @@ function setSVGSize(el) {
 setSVGSize('polyline');
 
 // GSAP work ////////////////////////////
-gsap.set(polyline, {drawSVG: '0% 2%'})
+gsap.set(polyline, {drawSVG: '0% 5%'})
 let tl = gsap.timeline();
-    tl.to(polyline, { duration: 12, ease: 'none', drawSVG: '98% 100%'});
+    tl.to(polyline, { duration: 10, ease: 'none', drawSVG: '95% 100%'});
 
 document.addEventListener('click', e => {
-    console.log(e.target);
+    console.log(e.target.getBoundingClientRect());
 })

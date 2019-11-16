698d40fe5fb9b2cfc06368d9ba1729a1b96313f5
joematune <joematune@gmail.com>
2019-11-16 18:53:36 +0100

    Add makePoints() for filling random polyline

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ console.log('happy');
 var vw = window.innerWidth / 100;
 var vh = window.innerHeight / 100;
 
-let line = document.querySelector('line');
-line.setAttribute('x1',10*vw);
-line.setAttribute('x2',10*vw);
-line.setAttribute('y1',10*vh);
-line.setAttribute('y2',90*vh);
+function makePoints(num) {
+    let points = '';
+    for (i = 0; i <= num; i++) {
+        let y = Math.floor(Math.random() * 100*vh);
+        let x = Math.floor(Math.random() * 100*vw);
+        points += `${x},${y} `
+    }
+    return points;
+}
+
+let points = makePoints(45);
+let polyline = document.querySelector('polyline');
+polyline.setAttribute('points', points);
 
 function showSVG(el) {
     // Prevents a flash of unstyled SVG
@@ -21,11 +29,13 @@ function setSVGSize(el) {
     el.style.height = window.innerHeight - 1;
     showSVG(el);
 }
-setSVGSize('line');
+setSVGSize('polyline');
 
 // GSAP work ////////////////////////////
-
+gsap.set(polyline, {drawSVG: '0% 2%'})
 let tl = gsap.timeline();
-    tl.to(line, { duration: 1, x: 90 })
-      .to(line, { duration: 3, y: 50 })
-      .to(line, { duration: 1, drawSVG: 0 })
+    tl.to(polyline, { duration: 12, ease: 'none', drawSVG: '98% 100%'});
+
+document.addEventListener('click', e => {
+    console.log(e.target);
+})

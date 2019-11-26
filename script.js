e2f56ef96a32beb309ca8f2d3886a999817213ec
joematune <joematune@gmail.com>
2019-11-26 19:03:14 +0100

    Add import for  polyline.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import Polyline from './polyline.js';
+
 const colors = [`#76c4ae`, `#9fc2ba`, `#bee9e4`, `#7ce0f9`, `#caeccf`,
                 `#d3d2b5`, `#cabd80`, `#e1ceb1`, `#ddb0a0`, `#d86c70`]
 function randomColor(arr) {
@@ -9,64 +11,14 @@ function randomColor(arr) {
 var vw = window.innerWidth / 100;
 var vh = window.innerHeight / 100;
 
-let lineId = 000;
+// parseInt() included due to 'use strict;' mode disallowing octals
+let lineId = parseInt('000', 10);
 function padId(id) {
     id++;
     if (id.toString().length < 3) {
         return id.toString().padStart(3, '0');
     } else {
         return id.toString();
-    }
-}
-
-// function makePoint(minX, width, minY, height) {
-//     let y = minY + Math.floor(Math.random() * height -5);
-//     let x = minX + Math.floor(Math.random() * width -5);
-//     return `${x},${y} `
-// }
-// Der anfang ist die ende und der ende ist der anfang.
-// function makePointLoop(num, minX, width, minY, height) {
-//     let anfangEnde = makePoint(minX, width, minY, height);
-//     let points = '';
-//     for (let i = 0; i <= num; i++) {
-//         points += makePoint(minX, width, minY, height);
-//     }
-//     return anfangEnde + points + anfangEnde;
-// }
-// get card bounding box
-// let box = document.getElementById('box-city').getBoundingClientRect();
-// let points = makePointLoop(45, box.x, box.width, box.y, box.height);
-// let polyline = document.createElementNS('http://www.w3.org/2000/svg',
-//                                         'polyline');
-// polyline.setAttribute('id', padId(lineId));
-// polyline.setAttribute('points', points);
-// document.querySelector('svg').appendChild(polyline);
-
-class Polyline {
-    constructor(num, boundingBox, id) {
-        this.num = num;
-        this.box = document.getElementById(boundingBox).getBoundingClientRect();
-        this.id = id;
-    }
-    makePoint(minX, width, minY, height) {
-        let y = minY + Math.floor(Math.random() * height -5);
-        let x = minX + Math.floor(Math.random() * width -5);
-        return `${x},${y} `
-    }
-    makePointLoop(num, minX, width, minY, height) {
-        let anfangEnde = this.makePoint(minX, width, minY, height);
-        let points = '';
-        for (let i = 0; i <= num; i++) {
-            points += this.makePoint(minX, width, minY, height);
-        }
-        return anfangEnde + points + anfangEnde;
-    }
-    makePolyline() {
-        let poly = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-        let { num, box, id } = this;
-        poly.setAttribute('id', id);
-        poly.setAttribute('points', this.makePointLoop(num,  box.x, box.width, box.y, box.height));
-        document.querySelector('svg').appendChild(poly);
     }
 }
 
